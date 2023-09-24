7a74fadd3afb1dab454f9488d514a2329aa22390
Paxsma <Paxsma>
2023-09-24 13:16:16 -0400

    Add project files.

---
+++ LUA-5.3.6/disassembler/imports.hpp
@@ -0,0 +1,9 @@
+extern "C" {
+#include "../lua/lauxlib.h"
+#include "../lua/lcode.h"
+#include "../lua/lobject.h"
+#include "../lua/lopcodes.h"
+#include "../lua/lstate.h"
+#include "../lua/lua.h"
+#include "../lua/lualib.h"
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Kosmic
+
+## Description
+
+Family of disassembler.
+
+## Usage
+
+Example usage can be found at each project in the solution
+
+## Disassemblers
+
+[LUA-5.2.4](LUA-5.2.4/)
+[LUA-5.3.6](LUA-5.3.6/)
